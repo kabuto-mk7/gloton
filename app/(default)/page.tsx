28e9f34fa650723ed97494f9fdf6953b7cf1188f
kb <kemalmehmetmk7@gmail.com>
2024-09-04 16:00:08 +0000

    added textbaes

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -8,12 +8,14 @@ import Features from "@/components/features";
 import Newsletter from "@/components/newsletter";
 import Zigzag from "@/components/zigzag";
 import Testimonials from "@/components/testimonials";
+import Brands from "@components/Brands";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Features />
+      <Brands />
       <Zigzag />
       <Testimonials />
       <Newsletter />
